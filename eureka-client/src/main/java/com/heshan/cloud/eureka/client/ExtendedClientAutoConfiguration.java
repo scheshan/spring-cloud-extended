e8e731227505122ffc9f23a7db4f0f334ce1c1af
scheshan <5373827@qq.com>
2020-02-15 19:53:42 +0800

    basic logic for client

---
+++ eureka-client/src/main/java/com/heshan/cloud/eureka/client/ExtendedClientAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.heshan.cloud.eureka.client;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ExtendedClientAutoConfiguration
+ *
+ * @author heshan
+ * @date 2020/2/15
+ */
+@Configuration
+public class ExtendedClientAutoConfiguration {
+}
