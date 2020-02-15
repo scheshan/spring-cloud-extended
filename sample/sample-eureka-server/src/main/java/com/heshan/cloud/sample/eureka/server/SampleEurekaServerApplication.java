40b97e63a5554741a8192795535e14699d93634b
scheshan <5373827@qq.com>
2020-02-15 18:28:26 +0800

    fix typo

--- sample/sample-eureka-server/src/main/java/com/heshan/cloud/sample/eureka/server/SampleEurekaServerApplication.java
+++ sample/sample-eureka-server/src/main/java/com/heshan/cloud/sample/eureka/server/SampleEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.heshan.cloud.sampe.eureka.server;
+package com.heshan.cloud.sample.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
